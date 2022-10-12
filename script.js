c3de3e17c465708c3bd30ec86c03f8352a18c198
wasihun-code <wasihunageru7947@gmail.com>
2022-10-12 23:59:58 +0530

    Added grid and clear function for grid

--- script.js
+++ script.js
@@ -1,31 +1,112 @@
+var sizeGrid = 16;
+var gridWidth = 20;
+var containerWidth = (sizeGrid * gridWidth) + "px";
+
+
+// Creat container for the grids and make it visible
 let container = document.createElement('div');
-container.classList.add('container');
-container.style.display = "flex";
-container.style.flexDirection = "column";
-
-let sizeGrid = 16;
-
-for (let i = 1; i <= sizeGrid; i++) {
-
-    let rowContainer = document.createElement('div');
-    rowContainer.style.display = "flex";
-    rowContainer.style.flexDirection = "row";
-
-    for (let j = 1; j <= sizeGrid; j++) {
-        let div = document.createElement('div');
-        div.classList.add("box-" + i + "-" + j);
-        div.innerText = j;
-        div.style.backgroundColor = "violet";
-        div.style.border = "2px solid red";
-        rowContainer.appendChild(div);
-    }
-    container.appendChild(rowContainer);
-}
-
-console.log(container);
 document.body.append(container);
 
+// Call to the createGrid function
+createGrid();
 
-container.addEventListener('mouseover', function(e) {
+// Select all boxes in the grid
+let divList = document.querySelectorAll('.box');
+
+// Style Each box in the grid
+divList.forEach(styleDivs);
+function styleDivs(item) {
+    item.style.backgroundColor = "#DCDCDC";
+    // item.style.border = "2px solid blue";
+    item.style.width = gridWidth + "px";
+    item.style.height = gridWidth + "px";
+}
+
+// Select Every row of the grid
+let rowContainer = document.querySelectorAll('.rowContainer');
+
+// Style Container for each row
+rowContainer.forEach(stylerowContainer);
+function stylerowContainer(item) {
+    item.style.width = containerWidth;
+    item.style.cssText = `
+        display: flex;
+        flex-direction: row;
+        background-color: black;
+    `
+}
+
+// Style Continaer for matrix of girds
+container.classList.add('container');
+container.style.cssText = `
+    display: flex;
+    flex-direction: column;
+    background-color: red;
+    border: 1px solid black;
+`
+container.style.width = containerWidth;
+
+// Change background of grid when mouse is hovered on it
+container.addEventListener('mouseover', function (e) {
     e.target.style.backgroundColor = "grey";
 })
+
+/*
+ * Button: CLEAR
+ */
+// Creat button for changing the grid
+let buttonClear = document.createElement('button');
+buttonClear.innerText = "Clear";
+document.body.append(buttonClear);
+
+// Style Button
+buttonClear.style.cssText = `
+    width: 60px;
+    height: 30px;
+    border-radius: 5px;
+    color: white;
+    background-color: #52b2bf;
+`;
+
+// Revert background to white when clear button is pressed
+buttonClear.addEventListener('click', function (e) {
+    divList.forEach(styleDivs);
+})
+
+// Create button to change the grid size
+let buttonChangeGrid = document.createElement('button');
+buttonChangeGrid.innerText = "Change Grid";
+document.body.append(buttonChangeGrid);
+buttonChangeGrid.style.cssText = `
+    width: 120px;
+    height: 30px;
+    border-radius: 5px;
+    color: white;
+    background-color: #52b2bf;
+`;
+
+buttonChangeGrid.addEventListener('click', function () {
+    sizeGrid = parseInt(prompt("Enter the number of grid desired: ", 16));
+});
+
+
+
+function createGrid() {
+    for (let i = 1; i <= sizeGrid; i++) {
+
+        // Create row container for flex row style
+        let rowContainer = document.createElement('div');
+        rowContainer.classList.add('rowContainer');
+
+        for (let j = 1; j <= sizeGrid; j++) {
+            // Create each box and append it to the rowcontainer
+            let div = document.createElement('div');
+            div.classList.add("box");
+            rowContainer.appendChild(div);
+        }
+
+        // Append each row to the container to creat matrix
+        container.appendChild(rowContainer);
+    }
+}
+
